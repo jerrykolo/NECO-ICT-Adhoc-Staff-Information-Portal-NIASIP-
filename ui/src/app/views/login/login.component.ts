f625cf7af782b03206fc6cfa099a1a51f526a96f
koloj <jerrytk63@gmail.com>
2026-07-21 17:54:02 +0100

    Refine password toggle button positioning and styles in login component

--- ui/src/app/views/login/login.component.ts
+++ ui/src/app/views/login/login.component.ts
@@ -184,7 +184,7 @@ import { AuthService } from '../../_core/services/auth.service';
     .toggle-password,
     button.toggle-password {
       position: absolute !important;
-      right: 14px !important;
+      right: 20px !important;
       top: 50% !important;
       transform: translateY(-50%) !important;
       display: flex !important;
